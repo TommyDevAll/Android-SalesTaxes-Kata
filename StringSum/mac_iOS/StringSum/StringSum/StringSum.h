9eeb4e7e3b3baa3cdc2268b32a1bfcb3c91655ea
Tommaso Resti <tommaso.resti@lastminute.com>
2016-10-12 17:02:11 +0200

    first dummy test

---
+++ StringSum/mac_iOS/StringSum/StringSum/StringSum.h
@@ -0,0 +1,11 @@
+//
+// Created by Tommaso Resti on 12/10/16.
+// Copyright (c) 2016 Tommaso Resti. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface StringSum : NSObject
++ (NSString *)sum:(NSString *)string with:(NSString *)with;
+@end
---
+++ StringSum/mac_iOS/StringSum/StringSum/StringSum.mm
@@ -0,0 +1,15 @@
+//
+// Created by Tommaso Resti on 12/10/16.
+// Copyright (c) 2016 Tommaso Resti. All rights reserved.
+//
+
+#import "StringSum.h"
+
+
+@implementation StringSum
+
++ (NSString *)sum:(NSString *)string with:(NSString *)with {
+  return @"2";
+}
+
+@end
