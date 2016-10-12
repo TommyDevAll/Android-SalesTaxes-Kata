bc9327e55d5ea53cade350fea4f40062b0da8d93
Tommaso Resti <tommaso.resti@lastminute.com>
2016-10-12 16:45:15 +0200

    Empty project

---
+++ StringSum/mac_iOS/StringSum/StringSum/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StringSum
+//
+//  Created by Tommaso Resti on 12/10/16.
+//  Copyright (c) 2016 Tommaso Resti. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+
+    return 0;
+}
